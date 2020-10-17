807e93fbd229aa5f7d5af754cdbf5f8b5e649df6
juliastefanoni <juliastefanonidev@gmail.com>
2020-10-16 23:53:50 -0300

    feat: update users route

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -68,6 +68,38 @@ class UserController {
       userInfos
     }
   }
+
+  async update({ params, request, response }) {
+    const user = await User.findBy('uuid', params.id)
+    const { email, name, state, telephone, address } = request.all()
+
+    if (user.email !== email) {
+      const userExists = await User.findBy('email', email)
+
+      if (userExists) {
+        return response.status(401).send('User already exists')
+      }
+    }
+
+    user.merge({ email: email, name: name })
+    await user.save()
+    await user
+      .userInfo()
+      .where('user_id', user.id)
+      .update({ state, telephone, address })
+
+    const userInfos = await user
+      .userInfo()
+      .setHidden(['created_at', 'updated_at', 'user_id', 'id'])
+      .fetch()
+
+    return {
+      uuid: params.id,
+      email: email,
+      name: name,
+      userInfos
+    }
+  }
 }
 
 module.exports = UserController
